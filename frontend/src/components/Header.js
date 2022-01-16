c73a2482059cc331cf9ee7a9863fa4c37436f565
saish0 <patrickvalera50@yahoo.com>
2022-01-16 09:32:30 -0800

    gallery

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,14 +1,31 @@
 import React, { useState } from 'react'
-import {Box,Button,Container, Typography} from '@mui/material'
+import { Box, Button, Container, Typography } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
 const Header = () => {
-    const [visible,setVisible]=useState(true)
+    const navigate = useNavigate()
+    const [visible, setVisible] = useState(true)
+    const [currentPage, setCurrentPage] = useState(window.location.pathname)
     return (
-        <Box component='header' display={visible?'block':'none'} sx={{py:{xs:1,md:2},bgcolor:'primary.main',width:'100%'}}>
-            <Container maxWidth='xl' sx={{display:'block'}}>
-            
-                <Typography variant='h2'><i className="fas fa-moon"></i> Spacetagram</Typography>
-                <Typography variant='body2' color='grey.400' sx={{ml:{xs:'2rem',md:'3rem'},fontSize:{xs:'.45rem',md:'.7rem'}}}> NASA's photo of the day</Typography>
+        <Box component='header' display={visible ? 'block' : 'none'} sx={{ py: { xs: 1, md: 2 }, bgcolor: 'primary.main', width: '100%' }}>
+            <Container maxWidth='xl' sx={{ display: 'flex', alignItems: 'center' }}>
+                <Box sx={{ flexGrow: '1' }}>
+                    <Button variant='text' onClick={() => navigate('/')} sx={{ py: '0' }}><Typography variant='h2' color='white'><i className="fas fa-moon"></i> Spacetagram</Typography></Button>
+                    <Typography variant='body2' color='grey.400' sx={{ ml: { xs: '2rem', md: '3rem' }, fontSize: { xs: '.45rem', md: '.7rem' } }}> NASA's photo of the day</Typography>
+                </Box>
+                <Box>
+                    {currentPage === '/' ?
+                        <Button variant='contained' onClick={() => {
+                            navigate('/gallery')
+                            setCurrentPage('gallery')
+                        }}><Typography variant='h4' ><i className="fas fa-th"></i></Typography></Button>
+                        :
+                        <Button variant='contained' onClick={() => {
+                            navigate('/')
+                            setCurrentPage('/')
+                        }}><Typography variant='h4' ><i className="fas fa-image"/></Typography></Button>
+                    }
 
+                </Box>
             </Container>
         </Box>
 
---
+++ frontend/src/screens/Gallery.js
@@ -0,0 +1,59 @@
+import { Container, Fade, ImageList, ImageListItem, Typography } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+
+const Gallery = () => {
+    const URL = 'https://api.nasa.gov/planetary/apod?api_key=l7jgCPMiMB7154fyZifWUm5LpKGi4YDiDIt92cgr&date='
+    // {"2022-01-15":{"date":"2022-01-15","liked":true,"url":"https://apod.nasa.gov/apod/image/2201/PIA19048europa1024.jpg","type":"image"},"2022-01-14":{"date":"2022-01-14","liked":true,"url":"https://apod.nasa.gov/apod/image/2201/NGC1566LRGBHa-Hanson-SelbyFinal1024.jpg","type":"image"},"2022-01-13":{"date":"2022-01-13","liked":true,"url":"https://apod.nasa.gov/apod/image/2201/HOOClassicBinned_ps1024.jpg","type":"image"},"2022-01-10":{"date":"2022-01-10","liked":true,"url":"https://www.youtube.com/embed/RtDSxi-D4KA?rel=0","type":"video"},"2022-01-11":{"date":"2022-01-11","liked":true,"url":"https://apod.nasa.gov/apod/image/2201/OrionStarFree3_Harbison_1080.jpg","type":"image"},"2022-01-08":{"date":"2022-01-08","liked":true,"url":"https://apod.nasa.gov/apod/image/2201/QuadrantidsnorthernskyRadioTelescopeArray1024.jpg","type":"image"},"2022-01-05":{"date":"2022-01-05","liked":true,"url":"https://apod.nasa.gov/apod/image/2201/SunriseYear_Vanzella_960.jpg","type":"image"},"2021-12-29":{"date":"2021-12-29","liked":true,"url":"https://apod.nasa.gov/apod/image/2112/JupiterStorms_JunoGill_1024.jpg","type":"image"}}
+    const [images, setImages] = useState([])
+    // console.log('IN UE')
+    let obj = JSON.parse(localStorage.getItem('likedImgs')) || {}
+    let ar = images
+    for (const property in obj) {
+        console.log(property)
+        ar.push(obj[property])
+    }
+    useEffect(() => {
+        setImages(ar)
+        return () => setImages([])
+    }, [])
+    return (
+        <Fade in={images}>
+            <Container maxWidth='xl' sx={{ minHeight: '100vh' }}>
+                <Typography variant='h2' sx={{ pt: '40px', textAlign: 'center' }}>
+                    {images[0]?'GALLERY':'GALLERY EMPTY'}
+                </Typography>
+                <ImageList variant="masonry" cols={3} gap={8}>
+                    {images && images.map((image) => {
+                        return (
+                            <Fade in={true}>
+                            <ImageListItem key={image.date}>
+                                    {image.type === 'image' &&
+                                        <img
+                                            src={image.url}
+                                            // srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                                            alt={image.date}
+                                            loading="lazy"
+                                            style={{ width: '248' }}
+                                            // onLoad={() => setImgLoaded({...imgLoaded,image:true})}
+                                        />
+                                        // :
+                                        // <iframe
+                                        //     style={{ width: '400' }}
+                                        //     src={image.url}
+                                        //     frameBorder="0"
+                                        //     title="Embedded youtube"
+                                        //     loop
+                                        // />
+                                    }
+
+                            </ImageListItem>
+                            </Fade>
+                        )
+                    })}
+                </ImageList>
+            </Container>
+        </Fade>
+    )
+}
+
+export default Gallery
